195ccd3e335103afef0bd6aff78f05527cc7dff4
Sanket-A <be2018se600@gces.edu.np>
2024-05-11 15:37:53 +0545

    Completed

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './WeatherApp.css'
 import search_icon from '../Assets/search.png'
 import clear_icon from '../Assets/clear.png'
@@ -11,15 +11,72 @@ import humidity_icon from '../Assets/humidity.png'
 const WeatherApp = () => {
 
     let api_key = "803e4ec5afb186eee0b323be8f83777f";
-    const search = () =>{
+    const [wicon, setWicon] = useState(cloud_icon);
+    const search = async  () =>{
 
         const element = document.getElementsByClassName('cityInput')
         if(element[0].value==="")
             {
                 return 0;
             }
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+            let response = await fetch(url);
+    let data = await response.json();
+    const humidity = document.getElementsByClassName("humidity-percent");
+    const wind = document.getElementsByClassName("wind-rate");
+    const temprature = document.getElementsByClassName("weather-temp");
+    const location = document.getElementsByClassName("weather-location");
 
+    humidity[0].innerHTML = data.main.humidity+" %";
+            wind[0].innerHTML = Math.floor(data.wind.speed)+" km/h";
+            temprature[0].innerHTML = data.main.temp+" C ";
+            location[0].innerHTML = data.name;
+
+
+      if (data.weather[0].icon==='01d' || data.weather[0].icon==='01n'){
+        setWicon(clear_icon);
+      }
+      else if
+        (data.weather[0].icon==='02d' || data.weather[0].icon==='02n'){
+            setWicon(cloud_icon);
+        
     }
+
+      
+      else if
+        (data.weather[0].icon==='03d' || data.weather[0].icon==='03n'){
+            setWicon(drizzle_icon);
+        
+    }
+      else if
+        (data.weather[0].icon==='04d' || data.weather[0].icon==='04n'){
+            setWicon(drizzle_icon);
+        
+    }
+      else if
+        (data.weather[0].icon==='09d' || data.weather[0].icon==='09n'){
+            setWicon(rain_icon);
+        
+    }
+      else if  (data.weather[0].icon==='010d' || data.weather[0].icon==='010n'){
+            setWicon(rain_icon);
+        
+    }
+       else if (data.weather[0].icon==='013d' || data.weather[0].icon==='013n'){
+            setWicon(snow_icon);
+        
+    }
+
+    else{
+        setWicon(clear_icon);
+    }
+
+
+        
+
+      
+    };
+          
   return (
     <div className='container'>
         <div className="top-bar">
@@ -29,7 +86,7 @@ const WeatherApp = () => {
             </div>
         </div>
 <div className="weather-image">
-    <img src={cloud_icon} alt=''/>
+    <img src={wicon} alt=''/>
 
 </div>
 <div className="weather-temp">
@@ -42,7 +99,7 @@ const WeatherApp = () => {
     <div className="element">
         <img src={humidity_icon} alt='' className='icon'/>
         <div className="data">
-            <div className="humidity-percentage">
+            <div className="humidity-percent">
                 64%
                 
             </div>
@@ -52,7 +109,7 @@ const WeatherApp = () => {
     <div className="element">
         <img src={wind_icon} alt='' className='icon'/>
         <div className="data">
-            <div className="humidity-percentage">
+            <div className="wind-rate">
                 18 km/hr%
                 
             </div>
